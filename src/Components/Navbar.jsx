8c820cc62f01ae356933b57c59a6ebfe7fc26bba
isroilovsaidahmad4-creator <isroilovsaidahmad4@gmail.com>
2025-10-08 12:30:03 +0500

    banner olib tashaldi

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <img style={{ width: "86%", height: "200px" }} className=' mr-36 mt-6' src="./Barner.png" alt="" />
+            {/* <img style={{ width: "86%", height: "200px" }} className=' mr-36 mt-6' src="./Barner.png" alt="" /> */}
         </div>
     )
 }
